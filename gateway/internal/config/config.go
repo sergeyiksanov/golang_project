2ac78aea689de8b51141ff4221e11aed95a7ef48
sergeyiksanov <sergeyiksanov25@gmail.com>
2025-02-04 22:27:53 +0300

    add config to gateway

---
+++ gateway/internal/config/config.go
@@ -0,0 +1,42 @@
+package config
+
+import (
+	"fmt"
+
+	"github.com/go-playground/validator/v10"
+	"github.com/spf13/viper"
+)
+
+var validate = validator.New()
+
+type Config struct {
+	Logger LoggerConfig `mapstructure:"logger"`
+}
+
+func Load() (*Config, error) {
+	v := viper.New()
+
+	v.SetDefault("logger.level", "info")
+	v.SetDefault("logger.format", "json")
+	v.SetDefault("logger.output_paths", []string{"stdout"})
+	v.SetDefault("logger.enable_stacktrace", false)
+
+	v.SetConfigName("local")
+	v.AddConfigPath("./config")
+	if err := v.ReadInConfig(); err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); !ok {
+			return nil, fmt.Errorf("config read error: %w", err)
+		}
+	}
+
+	var cfg Config
+	if err := v.Unmarshal(&cfg); err != nil {
+		return nil, fmt.Errorf("config unmarshal error: %w", err)
+	}
+
+	if err := validate.Struct(&cfg); err != nil {
+		return nil, fmt.Errorf("config validate error: %w", err)
+	}
+
+	return &cfg, nil
+}
